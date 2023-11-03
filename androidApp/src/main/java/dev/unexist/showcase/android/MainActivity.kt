46dee27c807c3df8fc7a80a2874f7af56c640886
Christoph Kappel <christoph@unexist.dev>
2023-11-03 15:24:58 +0100

    Updated greeting views

--- androidApp/src/main/java/dev/unexist/showcase/android/MainActivity.kt
+++ androidApp/src/main/java/dev/unexist/showcase/android/MainActivity.kt
@@ -3,11 +3,19 @@ package dev.unexist.showcase.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.unexist.showcase.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -27,14 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView(phrases: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(phrases) { phrase ->
+            Text(phrase)
+            Divider()
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView(listOf("Hello, Android!"))
     }
 }
